081cf1e56458dd781671e08965ea5e1e1fb81778
王耀民 <wangyaominde@qq.com>
2018-05-03 16:15:24 +0800

    Update main.py

--- esp8266/main.py
+++ esp8266/main.py
@@ -8,8 +8,7 @@ import time
 def sub_cb(topic, msg): 
    print(msg) 
 
-   
-   
+
 #====================connect wifi===============================#
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
@@ -20,37 +19,6 @@ while not sta_if.isconnected():
 print("Connected to Wifi\n") 
 
 
-
-
-
-
-
-
-'''#=======================flame======================#
-flame = machine.Pin(16, machine.Pin.IN)
-while True:
-	if(flame.value() == 0):
-		print("fire")
-	else:
-		print("no fire")
-	time.sleep(1)
-
-
-
-
-#===================dht11=========================================#
-d = dht.DHT11(machine.Pin(2))
-while True:
-	d.measure()		#celiang
-#	d.temperature()    # wendu
-#	d.humidity()		#shidu
-	print("wendu is %d shidu is %d"%(d.temperature(),d.humidity()))
-	time.sleep(1)
-
- 
- 
- 
- '''
 #==========================mqtt===============================#
 client = MQTTClient("client_name", "mqttserver",port=<portnum>) 
 client.set_callback(sub_cb) 
@@ -66,14 +34,3 @@ while True:
 	else:
 		client.publish(topic = "bysj",msg = "%d %d %d 0"%(d.temperature(),d.humidity(),mq.read()))
 	time.sleep(1)
-#    print("Sending ON")
-'''	if(flame.value() == 0):
-		client.publish(topic = "bysj/fire", msg = "fire")
-	else:
-		client.publish(topic = "bysj/fire", msg = "no fire")
-	client.publish(topic = "bysj/wendu",msg = "%d"%d.temperature())
-	client.publish(topic = "bysj/shidu",msg = "%d"%d.humidity())
-	client.publish(topic = "bysj/mq",msg = "%d"%mq.read())
-	print("Sending OFF") 
-	client.publish(topic="bysj", msg="0")
-	time.sleep(1) '''
