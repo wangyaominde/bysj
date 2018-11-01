33620c17aa3af9d8015b52c06ad0072f1159532d
王耀民 <wangyaominde@qq.com>
2018-11-01 13:49:04 +0800

    Update mqtt_with_db.py

--- centos/mqtt_with_db.py
+++ centos/mqtt_with_db.py
@@ -6,17 +6,17 @@ import re
 import pymysql
 
 def sjk():
-	db = pymysql.connect("localhost","****","******","BYSJ")
+	db = pymysql.connect("localhost","****","******","BYSJ")  #链接数据库
 	return db
 def xrsj(a):
 	db = sjk()
 	cursor = db.cursor()
-	cursor.execute('insert into bysj values("%d","%d","%d")'%(a[0],a[1],a[2]))
+	cursor.execute('insert into bysj values("%d","%d","%d")'%(a[0],a[1],a[2]))  #将温湿度和空气质量保存至数据库
 	db.commit()
 	db.close
 def int_c(msg):
 	ss = str(msg)
-	s = re.findall(r'\d+\.?d*',ss)
+	s = re.findall(r'\d+\.?d*',ss)   #拿到有用的数据
 	res1 = int(s[0])
 	res2 = int(s[1])
 	res3 = int(s[2])
@@ -24,16 +24,16 @@ def int_c(msg):
 	return (res1,res2,res3,res4)
 
 def sub():
-	msg = subscribe.simple("bysj", hostname="www.wangyaominde.cn",client_id="server")
+	msg = subscribe.simple("bysj", hostname="www.wangyaominde.cn",client_id="server")  #开启mqtt订阅
 	a = int_c(msg.payload)
 	return a
 
 while True:
 	a=sub()
 	if(a[3]==1):
-		fire = "fire"
+		fire = "fire"	#是否有火焰
 	else:
 		fire = "no fire"
 
 	xrsj(a)
-	print("wendu is :%d\tshidu is : %d\nmq is : %d\t%s"%(a[0],a[1],a[2],fire))
+	print("wendu is :%d\tshidu is : %d\nmq is : %d\t%s"%(a[0],a[1],a[2],fire))	#屏幕输出，调试用，后台可看
